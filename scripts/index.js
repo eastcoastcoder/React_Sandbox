0e186d57873ae5353245f8a193dcd538694780ce
Ethan Richardson <ethanx94@gmail.com>
2017-05-05 13:47:57 -0500

    prep for dudes

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,8 @@ class Quiz extends React.Component {
     super(props);
 
     this.state = {
+      counter: 1,
+      data: [],
       counter: 1
     };
 
@@ -15,13 +17,30 @@ class Quiz extends React.Component {
       this.setState({ counter: this.state.counter + 1 });
     }, 1000);
   }
+  
+//   componentWillMount() {
+//     fetch('https://graph.facebook.com/v2.9/1726444857365752/photos?fields=images&access_token=...')
+//     .then((response) => response.json())
+//     .then((body) =>
+//     this.setState({
+//       isLoading: false,
+//       data: body
+//     }))
+//     .catch(err => console.log(err));
+//   }
+  
   render() {
+    // if (this.state.isLoading) return <div>Loading...</div>;
     const styles = {
       color: this.props.foregroundColor,
       backgroundColor: this.props.backgrondColor
     };
+    // const results = this.state.data.data;
+    // const dude = results[(Math.random() * results.length | 0)];
+
     return (
       <div>
+        {/*<div><img src={dude.images[0].source} alt="IT IS WEDNESDAY" />)</div>*/}
         <CountDisplay style={styles} counter={this.state.counter} />
         <h1>{this.props.foregroundColor}</h1>
         {this.props.books.map((b) => <Book title={b} />)}
