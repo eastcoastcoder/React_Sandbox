70e0ccb8145c9aecd1ad3250aa8e64dc95a47bb3
Ethan Richardson <ethanx94@gmail.com>
2018-03-14 23:22:28 -0500

    topbanner work

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -1,10 +1,30 @@
 class TopBanner extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-        <MyList data={['Hi', 'I am', 'Ethan']} />
+      <div id="full-video">
+        <section className="video-section">
+          <div className="banner" id="home">
+            <video id="video_background" preload="auto" autoPlay loop>
+              <source src="src/assets/banner.mp4" type="video/mp4" />
+            </video>
+            <div className="top-header row" />
+            <div className="banner-info">
+              <div className="banner-left">
+                <img src="src/assets/1.png" alt="" />
+              </div>
+              <div className="banner-right">
+                <h1>ETHAN
+                  <br />RICHARDSON
+                </h1>
+                <div className="border" />
+                <h2>SOFTWARE ENGINEER</h2>
+              </div>
+              <div className="clearfix" />
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
 }
+// <MyList data={['Hi', 'I am', 'Ethan']} />
