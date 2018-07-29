7ab5dd82709befc7fb1af3ddd8e77d63b953e06e
Ethan Richardson <ethanx94@gmail.com>
2018-07-29 16:37:28 -0500

    blanked the project, reimplemented with webpack 4

--- src/index.js
+++ src/index.js
@@ -1,36 +1,27 @@
 import React from 'react';
-import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-import { Tabs, Tab } from 'react-materialize';
-import Main from './components/Main';
-import ClickDisplay from './components/ClickDisplay';
-// import CountDisplay from './components/CountDisplay';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import './css/index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <Router>
-    <div>
-      <Tabs className="tab-demo z-depth-1">
-        <Tab title="Main" active><Link to="/">Main</Link></Tab>
-        <Tab title="ClickDisplay"><Link to="/click">ClickDisplay</Link></Tab>
-        <Tab title="CountDisplay"><Link to="/count">CountDisplay</Link></Tab>
-      </Tabs>
-
-      <hr />
-
-      <Route exact path="/" component={Main} />
-
-      <Switch>
-        <Route exact path="/click" component={() => <ClickDisplay counter={0} />} />
-        <Route path="/click/:start" component={({ match }) => <ClickDisplay counter={Number(match.params.start)} />} />
-      </Switch>
-      {/* <Route path="/count" component={CountDisplay} /> */}
-    </div>
-  </Router>,
-    document.querySelector('#root')
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+ document.getElementById('root')
 );
 
+registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      rootElement
+    );
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,69 @@
-/**
- * WEBPACK CONFIG
- *
- * Notes on config properties:
- *
- * 'entry'
- * Entry point for the bundle.
- *
- * 'output'
- * If you pass an array - the modules are loaded on startup. The last one is exported.
- *
- * 'resolve'
- * Array of file extensions used to resolve modules.
- *
- * 'webpack-dev-server'
- * Is a little node.js Express server, which uses the webpack-dev-middleware to
- * serve a webpack bundle. It also has a little runtime which is connected to
- * the server via Socket.IO.
- *
- * 'webpack/hot/dev-server'
- * By adding a script to your index.html file and a special entry point in your
- * configuration you will be able to get live reloads when doing changes to your
- * files.
- *
- * devtool: 'eval-source-map'
- * http://www.cnblogs.com/Answer1215/p/4312265.html
- * The source map file will only be downloaded if you have source maps enabled
- * and your dev tools open.
- *
- * HotModuleReplacementPlugin()
- * Hot Module Replacement (HMR) exchanges, adds or removes modules while an
- * application is running without page reload.
- *
- * NoErrorsPlugin()
- * Hot loader is better when used with NoErrorsPlugin and hot/only-dev-server
- * since it eliminates page reloads altogether and recovers after syntax errors.
- *
- * 'react-hot'
- * React Hot Loader is a plugin for Webpack that allows instantaneous live
- * refresh without losing state while editing React components.
- *
- * 'babel'
- * Babel enables the use of ES6 today by transpiling your ES6 JavaScript into equivalent ES5 source
- * that is actually delivered to the end user browser.
- */
-
-const webpack = require('webpack');
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const appId = process.env.APPID;
+const appSecret = process.env.APPSECRET;
 
 module.exports = {
+  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
-    './src/index'
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index',
   ],
   output: {
-    path: __dirname,
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['', '.js']
-  },
-  devtool: 'eval-source-map',
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new Dotenv({
-      path: './.env' // Path to .env file (this is the default)
-      // safe: true // load .env.example (defaults to "false" which does not use dotenv-safe)
-    })
+    new webpack.DefinePlugin({
+      APPID: JSON.stringify(appId),
+      APPSECRET: JSON.stringify(appSecret),
+    }),
   ],
+  devServer: {
+    historyApiFallback: true,
+    inline: false,
+    port: 3000,
+    hot: true,
+  },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        loader: 'babel',
-        query:
-        {
-          presets: ['es2015', 'react']
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['react-hot-loader/babel'],
         },
-        include: path.join(__dirname, 'src')
-      }
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
     ]
   }
 };
