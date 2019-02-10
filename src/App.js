cb4fde21e19c591f35f54bf887af71b24a200f1a
Ethan Richardson <ethanx94@gmail.com>
2019-02-09 20:50:14 -0500

    complete HTML derived editor states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import { Button } from 'reactstrap';
 import draftToHtml from 'draftjs-to-html';
-// import htmlToDraft from 'html-to-draftjs';
+import htmlToDraft from 'html-to-draftjs';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorStateObj: {
-        description: EditorState.createEmpty(),
-        information: EditorState.createEmpty()
+      formData: {
+        description: '<p>Hey this <strong>editor</strong> rocks 😀</p>',
+        information: null,
       },
+      editorStateObj: { },
     };
+  }
+
+  componentDidMount() {
+    this.setUpEditorStates();
+  }
+
+  deriveHtmlData = (html) => {
+    const contentBlock = htmlToDraft(html);
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+      return EditorState.createWithContent(contentState);
+    } else {
+      return EditorState.createEmpty();
+    }
+  }
+
+  setUpEditorStates = () => {
+    const { formData, editorStateObj } = {...this.state};
+    for (const key of Object.keys(formData)) {
+      if(!formData[key]) {
+        editorStateObj[key] = EditorState.createEmpty();
+      } else {
+        editorStateObj[key] = this.deriveHtmlData(formData[key]);
+      }
+    }
+    this.setState({ editorStateObj });
   }
 
   onEditorStateChange = (editorState, key) => {
@@ -26,7 +53,7 @@ class App extends Component {
   };
 
   handleSubmit = () => {
-    const {editorStateObj} = this.state;
+    const { editorStateObj } = this.state;
     const resultArr = [];
     for (const key of Object.keys(editorStateObj)) {
       resultArr.push(draftToHtml(convertToRaw(editorStateObj[key].getCurrentContent())));
